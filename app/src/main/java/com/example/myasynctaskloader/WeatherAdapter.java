4be5e7ee2e17eb49c410ff5409600ee5ef9bee1c
dodo-nainggolan <valdo.henschel@gmail.com>
2019-11-01 17:36:20 +0700

    Initial commit #1

---
+++ app/src/main/java/com/example/myasynctaskloader/WeatherAdapter.java
@@ -0,0 +1,67 @@
+package com.example.myasynctaskloader;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
+    private ArrayList<WeatherItems> mData = new ArrayList<>();
+
+    public WeatherAdapter() {
+    }
+
+    public void setData(ArrayList<WeatherItems> items) {
+        mData.clear();
+        mData.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void addItem(final WeatherItems item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void clearData() {
+        mData.clear();
+    }
+
+    @NonNull
+    @Override
+    public WeatherViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.weather_items, viewGroup, false);
+        return new WeatherViewHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherViewHolder weatherViewHolder, int position) {
+        weatherViewHolder.textViewNamaKota.setText(mData.get(position).getName());
+        weatherViewHolder.textViewTemperature.setText(mData.get(position).getTemperature());
+        weatherViewHolder.textViewDescription.setText(mData.get(position).getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class WeatherViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewNamaKota;
+        public TextView textViewTemperature;
+        public TextView textViewDescription;
+
+        public WeatherViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNamaKota = itemView.findViewById(R.id.textKota);
+            textViewTemperature = itemView.findViewById(R.id.textTemp);
+            textViewDescription = itemView.findViewById(R.id.textDesc);
+        }
+    }
+}
